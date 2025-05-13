69d83b41fe2e193897fa8b7fba5a968c06e8d687
Elyes <65829912+EM51641@users.noreply.github.com>
2025-05-13 14:21:04 +0200

    changing `pyproject.toml`

--- varpy/data/SPX/__init__.py
+++ varpy/data/SPX/__init__.py
@@ -4,12 +4,11 @@ from varpy.data import load_file
 
 
 def load() -> DataFrame:
-
     """
     Load the sp500 data used in the examples
     Returns
     -------
     data : DataFrame
         Data set containing OHLC, adjusted close and the trading volume.
-    """ 
+    """
     return load_file(__file__, "SPX.csv.gz")
